3f6cede524e0d74511d9329945eab8d9172f7c02
imartinezl <inigomlap@gmail.com>
2019-06-28 18:57:39 +0200

    Updated for processed shp

--- map_draw_light.py
+++ map_draw_light.py
@@ -19,14 +19,9 @@ plt.tight_layout()
 ax.axis('off')
 #border.plot(edgecolor='white', lw=1, facecolor='#161616', ax = ax)
 
-for filename in Path('data').glob('**/rt_tramo_vial.shp'):
+for filename in Path('data/processed').glob('**/rt_tramo_vial.shp'):
     print(filename, str(round(os.path.getsize(filename)/1e6,2))+ "MB")
     df = gpd.read_file( filename )
-    clase_selection = [1001, 1002, 1005, 1003, 2000]
-    column_selection = ['clase','geometry']
-    df_filter = df[df.clase.isin(clase_selection)][column_selection]
-    df_filter.loc[df_filter.clase==1005, 'clase'] = 1003
-    df_filter.geometry = df_filter.geometry.simplify(tolerance=1, preserve_topology=False)
     df_filter.plot(color='black', lw=1, alpha=0.08, ax = ax)
     gc.collect()
 
